5f9f4b12a3613e8fac3a98ba304ceecb1bb7cba0
Jack Lund <jackl@geekheads.net>
2024-07-29 21:28:50 -0500

    Add code for GraphQL server

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,51 @@
+use async_graphql::http::GraphiQLSource;
+use async_graphql::*;
+use async_graphql_axum::GraphQL;
+use axum::{
+    response::{self, IntoResponse},
+    routing::get,
+    Router,
+};
+use tokio::net::TcpListener;
 use tokio::signal::unix::{signal, SignalKind};
+use tokio::sync::oneshot;
+
+// Dopey example schema
+struct Query;
+
+#[Object]
+impl Query {
+    /// Returns the sum of a and b
+    async fn add(&self, a: i32, b: i32) -> i32 {
+        a + b
+    }
+}
+
+async fn graphiql() -> impl IntoResponse {
+    response::Html(GraphiQLSource::build().endpoint("/").finish())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // An infinite stream of hangup signals.
     let mut sig = signal(SignalKind::hangup())?;
 
-    // Print whenever a HUP signal is received
-    loop {
+    // Signal handler thread - send on signal_rx if SIGHUP received
+    let (tx, signal_rx) = oneshot::channel();
+    tokio::spawn(async move {
         sig.recv().await;
-        break;
+        let _ = tx.send(());
+    });
+
+    let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
+    let app = Router::new().route("/", get(graphiql).post_service(GraphQL::new(schema)));
+
+    tokio::select! {
+        // Run the GraphQL server
+        _ = axum::serve(TcpListener::bind("127.0.0.1:8000").await?, app) => {}
+
+        // Listen for the HUP signal
+        _ = signal_rx => {}
     }
 
     Ok(())
